c72af75dc8e23cbc54cb7785b62ee0fb22925aed
Dwyane <92695412+DwyaneGOGO@users.noreply.github.com>
2022-12-06 21:56:22 -0500

    ...

--- 8Legs_Test.py
+++ 8Legs_Test.py
@@ -1,8 +1,8 @@
 from math import sin, cos
-from pylx16a.lx16a import *
+from pylx16a import *
 import time
 
-LX16A.initialize("/dev/ttyUSB0", 0.1)
+LX16A.initialize("ttyUSB0", 0.1)
 
 try:
     servo1 = LX16A(1)
